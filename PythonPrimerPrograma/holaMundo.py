1ecd631dec4df6d7cc0840afb23fd698fd4d0c29
AlejandroAlfedo <alejandro23aq01@hotmail.com>
2022-04-14 13:39:01 -0500

    Classic

---
+++ PythonPrimerPrograma/holaMundo.py
@@ -0,0 +1,8 @@
+print("Hola mundo")
+
+valorUno = int(input("Dame un valor: "))
+valorDos = int(input("Dame el segundo valor: "))
+
+resultado = valorUno + valorDos
+
+print("La suma es: " + str(resultado))
